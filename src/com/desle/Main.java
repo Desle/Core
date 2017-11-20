aa7f9dc6e57c680e26439a02bfb840640f5bac3b
Thom Wilbrink <thomwilbrink@ilumy.com>
2017-11-20 17:19:01 +0100

    Commiteroni

--- src/com/desle/Main.java
+++ src/com/desle/Main.java
@@ -1,20 +1,17 @@
 package com.desle;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-
 import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
-import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import com.desle.components.gui.modals.ModalCommandHandler;
+import com.desle.components.worlds.WorldInstance;
+
+import net.md_5.bungee.api.ChatColor;
 
 
 public class Main extends JavaPlugin implements Listener {
@@ -24,111 +21,53 @@ public class Main extends JavaPlugin implements Listener {
 		return (Main) Bukkit.getPluginManager().getPlugin("MCore");
 	}
 	
-	
 	@Override
 	public void onEnable() {
 		initializeCommands();
-		
+		Bukkit.getPluginManager().registerEvents(this, this);
 		
 		
 		for (Player player : Bukkit.getOnlinePlayers()) {
-			World world = Bukkit.createWorld(new WorldCreator("empty_world"));
-			
-			
-			Location from = player.getLocation();
-			Location to = world.getSpawnLocation();
-			
-			List<Chunk> chunksFrom = getSurroundingChunks(from);
-			chunksFrom.add(from.getChunk());
-			
-			List<Chunk> chunksTo = getSurroundingChunks(to);
-			chunksTo.add(to.getChunk());
-			
-			int maxY = (int) (from.getY() + 10);
-			int minY = (int) (from.getY() - 10);
-			
-			for (int x = 0; x < chunksFrom.size(); x++) {
-				Chunk chunkFrom = chunksFrom.get(x);
-				Chunk chunkTo = chunksTo.get(x);
-				
-				copyChunk(chunkFrom, chunkTo, minY, maxY);
-			}
-			
-			to.setY(player.getLocation().getY());
-			player.teleport(to);
+			player.sendMessage("why tf not working nib");
 		}
 	}
 	
 	
 	@Override
 	public void onDisable() {
+		for (WorldInstance worldInstance : WorldInstance.list) {
+			worldInstance.delete();
+		}
+	}
+	
+	
+	
+	@EventHandler
+	public void onKill(EntityDeathEvent e) {
+		Player player = e.getEntity().getKiller();
+		
+		player.sendMessage("test");
+		
+		World world = Bukkit.getWorld("world_template");
+		
+		new WorldInstance(world) {
+			
+			@Override
+			public void onReady(String worldName) {
+				World world = Bukkit.getWorld(worldName);
+				
+				if (world == null)
+					return;
+
+				Bukkit.broadcastMessage(worldName + ChatColor.GRAY + " created");
+				
+				player.teleport(world.getSpawnLocation());
+			}
+		};
 	}
 	
 	
 	public void initializeCommands() {
 		getCommand("finishmodal").setExecutor(new ModalCommandHandler());
-	}
-	
-	
-	public List<Chunk> getSurroundingChunks(Location location) {
-        int[] offset = {-1,0,1};
-
-        World world = location.getWorld();
-        int baseX = location.getChunk().getX();
-        int baseZ = location.getChunk().getZ();
-
-        List<Chunk> chunksAroundPlayer = new ArrayList<>();
-        for(int x : offset) {
-            for(int z : offset) {
-                Chunk chunk = world.getChunkAt(baseX + x, baseZ + z);
-                chunksAroundPlayer.add(chunk);
-            }
-        } return chunksAroundPlayer;
-    }
-	
-
-	private void copyChunk(Chunk chunk1, Chunk chunk2, int minY, int maxY){
-	    for (int x = 0; x < 16; x++) {
-	        for (int y = minY; y < maxY; y++) {
-	            for (int z = 0; z < 16; z++) {
-	            	Block from = chunk1.getBlock(x, y, z);
-	            	Block to = chunk2.getBlock(x, y, z);
-	            	
-	            	to.setBiome(from.getBiome());
-	            	to.setTypeIdAndData(from.getTypeId(), from.getData(), true);
-	            }
-	        }
-	    }
-	}
-	
-	
-	
-	
-	
-	
+	}	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ src/com/desle/components/worlds/WorldInstance.java
@@ -0,0 +1,130 @@
+package com.desle.components.worlds;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.fusesource.jansi.Ansi.Color;
+
+import com.desle.Main;
+
+public abstract class WorldInstance {
+	
+	public static List<WorldInstance> list = new ArrayList<WorldInstance>();
+	
+	private UUID uuid;
+	private String worldName;
+	private String originalWorldName;
+	private World world;
+	private boolean available;
+	
+	public WorldInstance(World world) {
+		this.uuid = UUID.randomUUID();
+		this.originalWorldName = world.getName();
+		this.worldName = this.originalWorldName + "_" + this.uuid.toString();
+		
+		copyWorldFile(world);
+		
+		list.add(this);
+	}
+	
+	
+	
+	private void copyWorldFile(World world) {
+		setAvailable(false);
+		
+		File from = world.getWorldFolder();
+		File to = new File(Bukkit.getWorldContainer(), this.worldName);
+		String worldName = this.worldName;
+		
+		System.out.println(worldName);
+
+		Bukkit.getScheduler().runTaskAsynchronously(Main.getPlugin(), new Runnable() {
+			@Override
+			public void run() {
+				try {
+					FileUtils.copyDirectory(from, to, new FileFilter() {
+					    public boolean accept(File pathname) {
+					        String name = pathname.getName();
+					        return (!name.equalsIgnoreCase("session.lock"));
+					    }
+					}, true);
+				} catch (IOException e) {
+					System.out.println(Color.RED + from.getName() + " could not be copied and caused an IOException.");
+				}
+				
+				setAvailable(true);
+				onReady(worldName);
+			}
+		});
+		
+	}
+	
+	public void setAvailable(boolean state) {
+		this.available = state;
+	}
+	
+	public boolean isAvailable() {
+		if (this.available && this.world == null)
+			return false;
+		
+		return true;
+	}
+	
+	public void unloadPlayerToWorld(Location location) {
+		if (location == null)
+			location = Bukkit.getWorlds().get(0).getSpawnLocation();
+		
+		for (Player player : this.world.getPlayers()) {
+			player.teleport(location);
+		}
+	}
+	
+	public boolean delete() {
+		this.setAvailable(false);
+		
+		if (this.world.getPlayers().size() > 0)
+			this.unloadPlayerToWorld(null);
+		
+		File file = Bukkit.getWorld(this.worldName).getWorldFolder();
+		Bukkit.unloadWorld(this.worldName, false);
+		
+		if (file.delete())
+			return true;
+		
+		return false;
+	}
+	
+	
+	
+	
+	
+	public String getWorldName() {
+		return this.worldName;
+	}
+	
+	public String getOriginalWorldName() {
+		return this.originalWorldName;
+	}
+	
+	public UUID getUUID() {
+		return this.uuid;
+	}
+	
+	public World getWorld() {
+		return this.world;
+	}
+	
+	
+	
+	
+	public abstract void onReady(String worldName);
+}
